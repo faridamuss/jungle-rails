fdf4ddf050873f76d56b659bbd840f8c7e9e428a
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-07 00:49:16 -0500

    Update categories with methods

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,29 @@
 class Admin::CategoriesController < ApplicationController
-end
+
+    def index
+      @categories = Category.order(id: :desc).all
+    end
+  
+    def new
+      @category = Category.new
+    end
+  
+    def create
+      @category = Category.new(category_params)
+  
+      if @category.save
+        redirect_to [:admin, :categories], notice: 'Category created!'
+      else
+        render :new
+      end
+    end
+  
+    private
+  
+    def cateory_params
+      params.require(:category).permit(
+        :name
+      )
+    end
+  
+  end
