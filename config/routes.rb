bac53c7a10d477cabe0505b69c6780313b7b0154
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-07 21:58:31 -0500

    Add login routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,29 @@ Rails.application.routes.draw do
 
   resources :about, only: [:index]
 
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
   
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
