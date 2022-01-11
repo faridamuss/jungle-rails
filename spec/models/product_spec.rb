6b2f429f9f18f6ac5fbf5abb8c85c974cde2fd49
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-11 00:38:44 -0500

    Update user.rb before running tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     let (:category) { Category.create(name: 'testCategory') }
-    subject { Product.new(name: 'test', price_cents: 9000, quantity: 100, category_id: category.id) }
+    subject { Product.new(
+      name: 'test', 
+      price_cents: 9000, 
+      quantity: 100, 
+      category_id: category.id) }
 
     it 'is valid with valid attributes' do
       expect(subject).to be_valid
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    subject do
+      User.new(
+        id: 1,
+        name: 'John',
+        email: 'johndoe@gmail.com',
+        password: 'password123',
+        password_confirmation: 'password123'
+      )
+    end
+    
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a email' do
+      subject.email = nil
+      expect(subject).to_not be_valid
+      print ".errors.full_messages === "
+      print subject.errors.full_messages
+    end
+
+  end
+
 end
+
