81718ffdc5929756f168374e47338858af8c1162
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-11 00:59:25 -0500

    Define new class method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 5 }
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by(email: email.strip.downcase)
+
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
 end
