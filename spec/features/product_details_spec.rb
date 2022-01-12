34522d659189fa067a9974461e1ce0a6475a7f09
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-11 19:58:03 -0500

    Feature Spec: product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,20 +1,56 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to details of single product", type: :feature, js: true do
+RSpec.feature "ProductDetails", type: :feature, js: true do
 
-   # SETUP
-   before :each do
-    @category = Category.create! name: 'Apparel'
+# SETUP
+before :each do
+  @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
+  10.times do |n|
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
   end
+end
+
+scenario " users can navigate from the home page to the product detail page by clicking on a product detail button" do
+  # ACT
+  visit root_path
+  first('article.product').find_link('Details').click
+
+  # DEBUG
+  # sleep 5
+  # puts page.html
+  # save_screenshot
+
+  # VERIFY
+  expect(page).to have_css 'article.product-detail', count: 1
+  save_screenshot
+end
+
+scenario " users can navigate from the home page to the product detail page by clicking on a product box" do
+  # ACT
+  visit root_path
+  first('article.product > header').click
+
+  # DEBUG
+  sleep 5
+  puts page.html
+  save_screenshot
+
+  # VERIFY
+  expect(page).to have_css 'article.product-detail', count: 1
+  save_screenshot
+end
+
 
 end
+
+# first('button.stripe-button-el').click
+# first('article.product').find_link('Add').click
+
+# visit '/cart'
