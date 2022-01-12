aa3387a02e15ec625e4fe5a32feadfd708b33142
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-11 19:48:09 -0500

    Copu setup code from the previous testing

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to details of single product", type: :feature, js: true do
+
+   # SETUP
+   before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
 end
