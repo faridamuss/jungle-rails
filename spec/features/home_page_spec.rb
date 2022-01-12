c2f2e312a7e0982316e6fcc6c7fa8565de303d16
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2022-01-11 19:37:14 -0500

    More robust test

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -17,13 +17,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   end
 
   scenario "They see all products" do
-    # ACT
     visit root_path
 
-    # DEBUG
-    save_screenshot
+    # commented out b/c it's for debugging only
+    # save_and_open_screenshot
 
-    # VERIFY
-    expect(page).to have_css 'article.product'
+    expect(page).to have_css 'article.product', count: 10
   end
-end
